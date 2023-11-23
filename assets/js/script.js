adf54f29de452f182bd222efc982e7cec2717d6f
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2023-11-23 13:11:54 -0800

    goes through questions - end game conditions not yet met

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const possAns = document.getElementById('options');
 const startButt = document.getElementById('start');
 const timerEl = document.getElementById('timeLeft');
 
-var i=0;
+var i = 0;
 
 let timeLeft;
 let questions = [
@@ -22,7 +22,7 @@ let allAnswers = [
     ['1. square brackets', '2. curly brackets', '3. parenthesis', '4. quotes'],
     ['1. declare', '2. var', '3. let', '4. const']
 ];
-let correctAns = ['3. alerts','2. parenthesis','4. all of the above','4. quotes','1. declare'];
+let correctAns = ['3. alerts', '2. parenthesis', '4. all of the above', '4. quotes', '1. declare'];
 
 // let question2 = 'The condition of an if/else statement is enclosed in:';
 // let answers2 = ['1. curly brackets', '2. parenthesis', '3. square brackets', '4. quotes'];
@@ -41,7 +41,7 @@ function startQuiz() {
 }
 
 function timer() {
-    var timeInterval = setInterval(function() {
+    let timeInterval = setInterval(function () {
         timerEl.textContent = timeLeft;
         timeLeft--;
         if (timeLeft === 0) {
@@ -49,34 +49,71 @@ function timer() {
             timerEl.textContent = 0;
             gameOver();
         }
-    },1000);
+    }, 1000);
 }
+
+// delay function from https://masteringjs.io/tutorials/fundamentals/wait-1-second-then
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
 
 // Displays Question & respective answer options
 function quiz() {
+    var ansButt = [];
+
     questionEl.textContent = questions[i];
     let answers = allAnswers[i];
-    for (var j=0; j < answers.length; j++) {
-        var ansButt = document.createElement("button");
-        possAns.append(ansButt);
-        ansButt.textContent = answers[j];
-    }
 
-    ansButt.addEventListener("click", function(ev) {
-        // ev.preventDefault();
-        if (ansButt.value === correctAns[i]) {
-            ansButt.setAttribute("background-color", "#70D500");
+    for (var j = 0; j < answers.length; j++) {
+        ansButt[j] = document.createElement("button");
+        possAns.append(ansButt[j]);
+        ansButt[j].textContent = answers[j];
+        ansButt[j].id = "ansButt" + j;
+    };
 
-        } else {
-            ansButt.setAttribute("background-color", "#D90E0E");
-        }
-    }
-    )
+    var ansButt1 = document.getElementById('ansButt0');
+    var ansButt2 = document.getElementById('ansButt1');
+    var ansButt3 = document.getElementById('ansButt2');
+    var ansButt4 = document.getElementById('ansButt3');
+
+    ansButt.forEach((element) =>
+        element.addEventListener("click", function () {
+            if (i < questions.length) {
+                if (element.textContent === correctAns[i]) {
+                    element.style.backgroundColor = "#70D500";
+                    i++;
+                    delay(500).then(() => {
+                        ansButt1.remove();
+                        ansButt2.remove();
+                        ansButt3.remove();
+                        ansButt4.remove();
+                        quiz();
+                    });
+                } else {
+                    element.style.backgroundColor = "#D90E0E";
+                    timeLeft = timeLeft - 5;
+                    i++;
+                    delay(500).then(() => {
+                        ansButt1.remove();
+                        ansButt2.remove();
+                        ansButt3.remove();
+                        ansButt4.remove();
+                        quiz();
+                    });
+                }
+            }
+            // if (i === questions.length - 1) {
+            //     clearInterval(timeInterval);
+            //     gameOver();
+            // }
+        })
+    );
 }
 
 function gameOver() {
-
+    questionEl.textContent = "Game Over";
 }
 
-startButt.addEventListener("click",startQuiz);
+startButt.addEventListener("click", startQuiz);
 
